9581c7061cff8fbac0dabe544dc2af7d00c01675
ls_yadu <116702012+Yadu0908@users.noreply.github.com>
2025-03-14 21:49:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import "./App.css";
 import "./Components/ReactCode/Basic"
 import Basic from "./Components/ReactCode/Basic";
+import "./Components/ReactCode/Login"
+import Login from "./Components/ReactCode/Login"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +12,8 @@ function App() {
   return (
     <>
       <main />
-      <Basic />
+      <Login />
+{/*       <Basic /> */}
       
     </>
   );
